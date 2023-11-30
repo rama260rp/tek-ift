d74431685946758fa789e2c2680f1e8fc3d1918c
ramazan260 <ramazan260@BPC06>
2023-11-30 13:08:48 +0300

    Proje dosyası ekle.

---
+++ tek çift/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tek_çift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayiTekmiCiftMi sayibul = new SayiTekmiCiftMi();
+            bool sonuc = sayibul.TekCift(3);
+            if (sonuc)
+            {
+                Console.WriteLine("Sayı tektir");
+            }
+            else
+            {
+                Console.WriteLine("sayı çifttir");
+            }
+
+
+        }
+    }
+    public class SayiTekmiCiftMi
+    {
+        public bool TekCift(int sayi)
+        {
+            if (sayi % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
